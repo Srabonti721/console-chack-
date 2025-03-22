26a058fd4436faed828cb4af1c06428d90a3425e
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-03-22 14:53:26 +0600

    consoel chack

---
+++ script/error.js
@@ -0,0 +1,11 @@
+const person ={
+    name:'hero',
+    age: 15,
+    ispassed : true,
+    conuntry : 'USA'
+}
+console.log(person);
+console.error('hellow');
+console.warn('hi');
+console.dir(person);
+console.table(person);
