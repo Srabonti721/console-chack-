2f1bbd607a6730fbbcf93af41fcdbb468c67c4dd
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-03-22 15:31:27 +0600

    debugging

--- script/error.js
+++ script/error.js
@@ -9,3 +9,31 @@ console.error('hellow');
 console.warn('hi');
 console.dir(person);
 console.table(person);
+
+const colors = ['white','green','red','purple','pink','gold','silver','blue','yellow','black']
+console.table(colors);
+console.log(colors[9]);
+// debugging
+const num1 = 10;
+const num2 = '20';
+console.log(num1,num2);
+const result = num1+ num2;
+console.log(result);
+// for loop
+for(let i = 0; i<5; i++){
+const element = i;
+console.log(element);
+}
+for(let j = 1; j<10; j++){
+    const tag = j;
+    debugger
+    console.log(tag);
+    
+}
+
+/**  conde ka debugge kore dekher jono amera jigula babher korta pare
+ * console
+ *  line ar maja debugger kore
+ * browser a giya sourse thaka debugge 
+ * forword traking
+*/
